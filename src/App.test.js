5308e2f2ad37c8d7e84f9ffb8c9a6acc733093e0
Alex Devero <deveroalex@gmail.com>
2019-09-20 05:50:15 +0200

    Add test for App component

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+
+import App from './App'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
+  )
+})
