6b0ecb30e16b2493c5840795bff6086d785bae25
Alex Devero <deveroalex@gmail.com>
2019-09-20 05:50:01 +0200

    Add index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+﻿import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import registerServiceWorker from './registerServiceWorker'
+
+import App from './App'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  document.getElementById('root')
+)
+
+registerServiceWorker()
