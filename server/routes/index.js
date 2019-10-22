6f12a675d0b44bb3db211fa42c9cefd660d50e93
Alex Devero <deveroalex@gmail.com>
2019-10-22 15:50:09 +0200

    Add index route

---
+++ server/routes/index.js
@@ -0,0 +1,20 @@
+﻿const express = require('express')
+
+const CONSTANTS = require('../constants')
+
+const sampleData = require('../sampleData')
+
+
+const router = express.Router()
+// MasterDetail Page Endpoint
+router.get(CONSTANTS.ENDPOINT.MASTERDETAIL, (req, res) => {
+  res.json(sampleData.textAssets)
+})
+
+// Grid Page Endpoint
+router.get(CONSTANTS.ENDPOINT.GRID, (req, res) => {
+  res.json(sampleData.textAssets)
+})
+
+
+module.exports = router
